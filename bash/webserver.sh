d109fcfb4ea88142a3e7fa770f5922dec7ad1c43
Maximizer02 <max.beller@yahoo.com>
2025-08-31 00:45:39 +0200

    make bash version serve from file

--- bash/webserver.sh
+++ bash/webserver.sh
@@ -1,15 +1,35 @@
 #! /bin/bash
+
+# exit if no servable html is found
+if [ ! -f "index.html" ] ; then
+		echo "no index.html file found";
+		exit 1;
+fi
+
 enable accept;
 while true
 do
-		accept -b 0.0.0.0 -v file 8080;
-		while read -u $file -r line
+		# establish tcp connection
+		accept -b 0.0.0.0 -v tcp 8080;
+		while read -u $tcp -r line
 		do
 				echo $line;
+				# read until blank line
 				if [ "$line" = $'\r' ] ; then
 						break;
 				fi;    
 		done;
-		echo -e "HTTP/1.1 200 OK\r\ncontent-type: text/html\r\n\r\n<h1>Hello from Bash!</h1>\r\n" >&"$file";
-		exec {file}>&-;
+		
+		# transmit header
+		echo -e "HTTP/1.1 200 OK\r\ncontent-type: text/html\r\n\r\n" >&"$tcp";
+
+		# transmit content
+		while read -r line
+		do
+				echo "$line" >&"$tcp";
+		done < "index.html";
+
+		# close connection
+		exec {tcp}>&-;
 done;
+
